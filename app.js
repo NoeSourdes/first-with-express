657604b29dce461dca8fdb95a2a2a1bf4d3c0905
Noé Sourdès <souredesnoe19@gmail.com>
2022-11-07 18:55:09 +0100

    systheme de page avec express-handlebars

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000 // ou 80
+//1)
 
-app.get('/', (req, res) => {
-    res.send('<h1>bonjour Lucas</h1>')
-})
+const express = require("express");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000; // ou 80
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>bonjour Lucas</h1>')
+// })
+
+// app.listen(port, () => {
+//     console.log('votre site est à jour sur le port 3000 !!');
+// })
+
+//2)
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "routes")));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Home",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+  });
+});
 
 app.listen(port, () => {
-    console.log('votre site est à jour sur le port 3000 !!');
-})
+  console.log("votre site est à jour sur le port 3000 !!");
+});
